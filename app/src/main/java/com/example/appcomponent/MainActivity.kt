ac00fa643e520c3ef846edc204f4b0c4f8523e20
joaomonsy <joao.lima548@etec.sp.gov.br>
2023-08-17 08:53:08 -0300

    App Components

---
+++ app/src/main/java/com/example/appcomponent/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.example.appcomponent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appcomponent.ui.theme.AppComponentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App(){
+    AppComponentTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column (
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ClickableOutlinedCardSample()
+                MenuTopBar()
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 250, heightDp = 250)
+@Composable
+fun AppPreview() {
+    App()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuTopBar(){
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Blue
+                ),
+                title = {
+                    Column {
+                        Text(text = "JetPack", color = Color.White)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.ArrowBack, tint = Color.White, contentDescription = "")
+                    }
+                },
+                actions = {
+                    Icon(imageVector = Icons.Filled.MoreVert, tint = Color.White, contentDescription = "")
+                }
+            )
+        }
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)){
+
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 200, heightDp = 70)
+@Composable
+fun MenuTopBarPreview() {
+    MenuTopBar()
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ClickableOutlinedCardSample() {
+    OutlinedCard(
+        onClick = { /* Do something */ },
+        modifier = Modifier.size(width = 180.dp, height = 100.dp)
+    ) {
+        Box(Modifier.fillMaxSize()) {
+            Text("Clickable", Modifier.align(Alignment.Center))
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 200, heightDp = 70)
+@Composable
+fun ClickableOutlinedCardSamplePreview() {
+    ClickableOutlinedCardSample()
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppComponentTheme {
+        Greeting("Android")
+    }
+}
